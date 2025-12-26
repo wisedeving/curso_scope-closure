ae6a1c4af2866795fe9ee48110708c71922fb9f3
Sebas Parra <jsparrac17@gmail.com>
2025-12-25 22:27:38 -0500

    Cambios

---
+++ src/scope/global.js
@@ -0,0 +1,18 @@
+// Variables
+
+//Global
+
+var fruit = 'Apple';
+
+function bestFruit(){
+    console.log(fruit);
+}
+
+bestFruit();
+
+function countries(){
+    country = 'Colombia';
+    console.log(country);
+}
+
+countries();
